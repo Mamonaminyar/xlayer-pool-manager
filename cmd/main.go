a236f7b03f2ddf18e1c9242a4b8f30cc54d52115
EricQ <50510214+xrqin@users.noreply.github.com>
2025-03-01 20:42:30 +0800

    Ericq/xlayer pool manager (#5)
    * add kms Aws api
    
    * add replace
    
    * add goin mod
    
    * add goin mod
    
    * add goin mod
    
    * add goin mod
    
    * add goin mod
    
    * add print info
    
    * remove replace in go.mod
    
    * add dockerfile.in
    
    * add go env in dockerfile.in
    
    * add go env in dockerfile.in
    
    * change replace and go mod version
    
    * move 'mkdir
    
    * change replace tag
    
    * add build.in
    
    * modify build.in && dockerfile.in
    
    * remove temp_src && goin.mod
    
    * remove zkevm_pool_manager bin
    
    * modify build.in && dockerfile.in && remove log
    
    * remove log

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/0xPolygonHermez/zkevm-data-streamer"
@@ -22,9 +23,11 @@ import (
 	server "github.com/0xPolygonHermez/zkevm-pool-manager/server"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/urfave/cli/v2"
+	kms "gitlab.okg.com/okcoin-commons/ok-kms-go-client/kms"
 )
 
 const appName = "zkevm-pool-manager"
+const encryptedPrefix = "{encrypt}"
 
 var (
 	configFileFlag = cli.StringFlag{
@@ -40,6 +43,21 @@ var (
 		Required: false,
 	}
 )
+
+func getDBPassword(dbPassword string) (string, error) {
+	if strings.HasPrefix(dbPassword, encryptedPrefix) {
+		if err := kms.Init(); err != nil {
+			return "", fmt.Errorf("failed to init KMS: %w", err)
+		}
+		secretKey := strings.TrimPrefix(dbPassword, encryptedPrefix)
+		realPass, err := kms.GetAwsSecretValue(secretKey)
+		if err != nil {
+			return "", fmt.Errorf("failed to fetch DB pass from KMS: %w", err)
+		}
+		return realPass, nil
+	}
+	return dbPassword, nil // Return original password if not encrypted
+}
 
 func main() {
 	app := cli.NewApp()
@@ -76,6 +94,11 @@ func start(cliCtx *cli.Context) error {
 	c, err := config.Load(cliCtx, true)
 	if err != nil {
 		return err
+	}
+
+	c.DB.Password, err = getDBPassword(c.DB.Password)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Setup logger
